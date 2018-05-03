efc7c14284af44a6e8da4d7f51bb1f2ae74bc415
Olivier Sarrat <olivier.sarrat@laposte.net>
2018-05-03 16:04:39 +0200

    New CRM controller with full external ID export

--- groupeurd_crm/controllers.py
+++ groupeurd_crm/controllers.py
@@ -14,7 +14,7 @@ class GroupeURD_CRM(http.Controller):
 	def view_mailing_lists(self, db):
 		http.request.session.db = db
 		http.request.cr.execute("""
-select mmml.name as mailing_list, imd.name as external_id
+select mmml.name as mailing_list, imd.module || '.' || imd.name as external_id
 from ir_model_data imd
 inner join mail_mass_mailing_list mmml on mmml.id = imd.res_id
 where imd.model = 'mail.mass_mailing.list'
@@ -31,7 +31,7 @@ order by mmml.name;
 	def view_partner_categories(self, db):
 		http.request.session.db = db
 		http.request.cr.execute("""
-select rpc.name as category, imd.name as external_id
+select rpc.name as category, imd.module || '.' || imd.name as external_id
 from ir_model_data imd
 inner join res_partner_category rpc on rpc.id = imd.res_id
 where imd.model = 'res.partner.category'
@@ -49,7 +49,7 @@ order by rpc.name;
 	def view_partner_titles(self, db):
 		http.request.session.db = db
 		http.request.cr.execute("""
-select rpt.name as title, imd.name as external_id
+select rpt.name as title, imd.module || '.' || imd.name as external_id
 from ir_model_data imd
 inner join res_partner_title rpt on rpt.id = imd.res_id
 where imd.model = 'res.partner.title'
@@ -67,7 +67,7 @@ order by rpt.name;
 	def view_languagelist(self, db):
 		http.request.session.db = db
 		http.request.cr.execute("""
-select gcl.name as language, imd.name as external_id
+select gcl.name as language, imd.module || '.' || imd.name as external_id
 from ir_model_data imd
 inner join groupeurd_crm_languagelist gcl on gcl.id = imd.res_id
 where imd.model = 'groupeurd_crm.languagelist'
